ffff01f16508dc2a527fb2f19735656d43d375b2
Arthur <crfernandesarthur@gmail.com>
2024-12-06 08:45:05 -0300

    feat: Implement errors tests

--- src/models/sqlite/repositories/users_repository_test.py
+++ src/models/sqlite/repositories/users_repository_test.py
@@ -1,5 +1,7 @@
+import pytest
 from unittest import mock
 from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from sqlalchemy.orm.exc import NoResultFound
 from src.models.sqlite.entities.users import UsersTable
 from .users_repository import UserRepository
 
@@ -10,11 +12,26 @@ class MockConnection:
             data = 
             [
                 (
-                    [mock.call.query(UsersTable)], # query
-                    [UsersTable(id=1, name="John Doe", username="johndoe")] # resultado
+                    [mock.call.query(UsersTable)],
+                    [UsersTable(id=1, name="John Doe", username="johndoe")] 
                 )
             ]
         )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+class MockConnectionNoResult:
+
+    def __init__(self):
+        self.session = UnifiedAlchemyMagicMock()
+        self.session.query.side_effect = self.__raise_no_result_found
+
+    def __raise_no_result_found(self, *args, **kwargs): 
+        raise NoResultFound("No result found")
 
     def __enter__(self):
         return self
@@ -33,3 +50,33 @@ def test_list_users():
     assert response[0].id == 1
     assert response[0].name == "John Doe"
     assert response[0].username == "johndoe"
+
+def test_list_users_no_result():
+    mock_connection_no_result = MockConnectionNoResult()
+    repo = UserRepository(mock_connection_no_result)
+    response = repo.list_users()
+
+    mock_connection_no_result.session.query.assert_called_once_with(UsersTable)
+    mock_connection_no_result.session.all.assert_not_called()
+
+    assert response == []
+
+def test_delete_user():
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
+
+    repo.delete_user(1)
+
+    mock_connection.session.query.assert_called_once_with(UsersTable)
+    mock_connection.session.filter.assert_called_once_with(UsersTable.id == 1)
+    mock_connection.session.delete.assert_called_once_with()
+    mock_connection.session.commit.assert_called_once_with()
+
+def test_delete_user_error():
+    mock_connection_no_result = MockConnectionNoResult()
+    repo = UserRepository(mock_connection_no_result)
+
+    with pytest.raises(Exception):
+        repo.delete_user(1)
+
+    mock_connection_no_result.session.rollback.assert_called_once()
